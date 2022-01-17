ae1b1450fd626dac080c725c940a832b59f21abf
Chanzik Brooks <chanzik01@gmail.com>
2022-01-17 13:10:34 -0500

    Startup code

---
+++ BoardingPass/src/main/java/BoardingPassTicket.java
@@ -0,0 +1,151 @@
+import java.util.Scanner;
+
+//Contains info needed to create a Boarding Pass Ticket
+public class BoardingPassTicket {
+    private String name;
+    private String email;
+    private String phoneNumber;
+    private int age;
+    private String gender;
+    private int passNumber;
+    private String date;
+    private String origin;
+    private String destination;
+    private String ETA; //Estimated Time of Arrival to destination.
+    private String departureTime; //Time for departure using plane, ship, etc.
+    private int totalTicketPrice;
+    Scanner sc = new Scanner(System.in);
+
+    public BoardingPassTicket() {
+
+    }
+
+    public BoardingPassTicket(String name, String email, String phoneNumber, int age, String gender, int passNumber, String date, String origin, String destination, String ETA, String departureTime, int totalTicketPrice) {
+        this.name = name;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.age = age;
+        this.gender = gender;
+        this.passNumber = passNumber;
+        this.date = date;
+        this.origin = origin;
+        this.destination = destination;
+        this.ETA = ETA;
+        this.departureTime = departureTime;
+        this.totalTicketPrice = totalTicketPrice;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public int getPassNumber() {
+        return passNumber;
+    }
+
+    public void setPassNumber(int passNumber) {
+        this.passNumber = passNumber;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getOrigin() {
+        return origin;
+    }
+
+    public void setOrigin(String origin) {
+        this.origin = origin;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    public String getETA() {
+        return ETA;
+    }
+
+    public void setETA(String ETA) {
+        this.ETA = ETA;
+    }
+
+    public String getDepartureTime() {
+        return departureTime;
+    }
+
+    public void setDepartureTime(String departureTime) {
+        this.departureTime = departureTime;
+    }
+
+    public int getTotalTicketPrice() {
+        return totalTicketPrice;
+    }
+
+    public void setTotalTicketPrice(int totalTicketPrice) {
+        this.totalTicketPrice = totalTicketPrice;
+    }
+
+    @Override
+    public String toString() {
+        return "BOARDING PASS TICKET" + "\n" +
+                "- Name = " + name + "\n" + "    |" + "\n" +
+                "- Email = " + email + "\n" + "    |" + "\n" +
+                "- PhoneNumber = " + phoneNumber + "\n" + "    |" + "\n" +
+                "- Age = " + age + "\n" + "    |" + "\n" +
+                "- Gender = " + gender + "\n" + "    |" + "\n" +
+                "- PassNumber = " + passNumber + "\n" + "    |" + "\n" +
+                "- Date = " + date + "\n" + "    |" + "\n" +
+                "- Origin = " + origin + "\n" + "    |" + "\n" +
+                "- Destination = " + destination + "\n" + "    |" + "\n" +
+                "- ETA = " + ETA + "\n" + "    |" + "\n" +
+                "- DepartureTime = " + departureTime + "\n" + "\n" +
+                "- TOTAL: " + totalTicketPrice
+                ;
+    }
+}
--- BoardingPass/src/main/java/Driver.java
+++ BoardingPass/src/main/java/Driver.java
@@ -1,5 +1,8 @@
 public class Driver {
     public static void main(String[] args) {
-        
+        BoardingPassTicket ticket = new BoardingPassTicket();
+
+        System.out.println("WELCOME!!!" + "\nWe require your information to create a ticket." + "\n");
+        System.out.println(ticket.toString());
     }
 }
