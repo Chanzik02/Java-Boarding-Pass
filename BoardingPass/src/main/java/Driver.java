b221c6855bd558e721c976913bc85870e228d557
Chanzik Brooks <chanzik01@gmail.com>
2022-01-17 16:56:36 -0500

    changed java cases layout

--- BoardingPass/src/main/java/Driver.java
+++ BoardingPass/src/main/java/Driver.java
@@ -5,11 +5,7 @@ public class Driver {
         System.out.println(ticket.toString());
         System.out.println("Branch Update");
 
-
-        System.out.println("Update 2#");
-
-        System.out.println("Making a change...");
-        System.out.println("Change to code");
+        System.out.println("changed test file layout");
 
 
 
