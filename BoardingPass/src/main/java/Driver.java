2bdadd827b14fa86c23305fed68a99a465530674
Chanzik Brooks <chanzik01@gmail.com>
2022-01-17 13:45:28 -0500

    test change

--- BoardingPass/src/main/java/Driver.java
+++ BoardingPass/src/main/java/Driver.java
@@ -1,11 +1,10 @@
 public class Driver {
     public static void main(String[] args) {
         BoardingPassTicket ticket = new BoardingPassTicket();
-
         System.out.println("WELCOME!!!" + "\nWe require your information to create a ticket." + "\n");
         System.out.println(ticket.toString());
         System.out.println("Branch Update");
-        System.out.println("Still in branch?");
+        System.out.println("Update 2#");
 
     }
 }
