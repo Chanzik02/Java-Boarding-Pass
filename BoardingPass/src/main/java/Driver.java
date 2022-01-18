1246f632950eed568da65d695e6500d854b11187
bradfan <bradley.tomlinson@genspark.com>
2022-01-18 07:42:49 -0700

    Testing and methods on getters

--- BoardingPass/src/main/java/Driver.java
+++ BoardingPass/src/main/java/Driver.java
@@ -2,14 +2,7 @@ public class Driver {
     public static void main(String[] args) {
         BoardingPassTicket ticket = new BoardingPassTicket();
         System.out.println("WELCOME!!!" + "\nWe require your information to create a ticket." + "\n");
-        System.out.println(ticket.toString());
-        System.out.println("Branch Update");
-
-
-        System.out.println("Update 2#");
-
-        System.out.println("Making a change...");
-        System.out.println("Change to code");
+        ticket.userInput();
 
 
 
