64029a30f2a5893cfb0e68fb6113c3ac09da524f
bradfan <bradley.tomlinson@genspark.com>
2022-01-18 13:51:43 -0700

    Add methods and tests Boarding Pass

--- BoardingPass/src/main/java/Driver.java
+++ BoardingPass/src/main/java/Driver.java
@@ -2,9 +2,8 @@ public class Driver {
     public static void main(String[] args) {
         BoardingPassTicket ticket = new BoardingPassTicket();
         System.out.println("WELCOME!!!" + "\nWe require your information to create a ticket." + "\n");
-        ticket.userInput();
-
-
+       ticket.userInput();
+//        System.out.println(ticket.formatDate());
 
     }
 }
