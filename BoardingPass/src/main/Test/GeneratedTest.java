9472cc175ff3db55d4276b4aac4142cdf9228c05
bradfan <bradley.tomlinson@genspark.com>
2022-01-21 07:23:28 -0700

    Added Tests to GenerateTest

--- BoardingPass/src/main/Test/GeneratedTest.java
+++ BoardingPass/src/main/Test/GeneratedTest.java
@@ -6,6 +6,8 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class GeneratedTest {
 
+    Generated generated = new Generated();
+
     @BeforeEach
     void setUp() {
     }
@@ -16,9 +18,14 @@ class GeneratedTest {
 
     @Test
     void ticketPrice() {
+        assertEquals(15, generated.ticketPrice(100), "Calculation incorrect");
+        assertNotEquals(20, generated.ticketPrice(100), "Checking for incorrect value.");
     }
 
     @Test
     void discount() {
+        assertEquals(100, generated.discount(200, 10, "M"), "Calculation incorrect (12 or younger.)");
+        assertEquals(80, generated.discount(200, 60, "F"), "Calculation incorrect (60 or older.)");
+        assertEquals(150, generated.discount(200, 40, "F"), "Calculation incorrect (Discount based on gender.)");
     }
 }
--- BoardingPass/src/main/java/Generated.java
+++ BoardingPass/src/main/java/Generated.java
@@ -1,21 +1,21 @@
 public class Generated {
-    BoardingPassTicket ticket = new BoardingPassTicket();
+
 
     public float ticketPrice(int mileage) {
         float price = (float) (mileage * .15);
         return price;
     }
 
-    public float discount(int price) {
-        int adjPrice = price;
-        if(ticket.getAge() <= 12) {
+    public float discount(int price, int age, String gender) {
+        float adjPrice = price;
+        if(age <= 12) {
             adjPrice = (int) (price * .5);
             return adjPrice;
         }
-        if(ticket.getAge() >= 60) {
+        if(age >= 60) {
             adjPrice = (int) (price * .4);
         }
-        if(ticket.getGender().equals("F") && ticket.getAge() > 12 && ticket.getAge() < 60) {
+        if(gender.equals("F") && age > 12 && age < 60) {
             adjPrice = (int) (price * .75);
         }
         return adjPrice;
